cb7902d9f807e40c1d2b9f6e3e8aa2068d2adffe
NikitaShchegolev <i@nnshchegolev.ru>
2025-12-05 17:23:47 +0300

    +++

---
+++ Teaching.Concurrency.Queue/Teaching.Concurrency.Queue/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Teaching.Concurrency.Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
